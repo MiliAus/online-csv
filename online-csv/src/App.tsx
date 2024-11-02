47e23cfc307396b2d1e34a5c6b9bbfe647ab3945
Austin Leverett <71096053+MiliAus@users.noreply.github.com>
2024-11-02 19:45:33 +0000

    quick commit

--- online-csv/src/App.tsx
+++ online-csv/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Sidebar from 'src/components/Navbar';
+import Navbar from 'src/components/Navbar';
 import './syles/App.css';
 
 const App = () => {
   return (
     <div>
-      <Sidebar />
+      <Navbar />
     </div>
   );
 };
