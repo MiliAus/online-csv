20f03101a1bebe07690fab8ee09e9a8718d8728d
Austin Leverett <71096053+MiliAus@users.noreply.github.com>
2024-11-08 16:02:24 +0000

    quick commit

--- online-csv/src/components/Projects.tsx
+++ online-csv/src/components/Projects.tsx
@@ -10,15 +10,15 @@ const Projects = () => {
     return (
       <>
       <div className='fourth'>
-      <h3>Projects</h3>
-      <p>Vito</p>
-      <p>Sunset public project focused on disability assistance. I worked on CI/CD processes such as setting up Github Actions and design choices for the frontend that utilized Swift UI.</p>
+      <h3 className='margins_small'>Projects</h3>
+      <p className='title'>Vito</p>
+      <p className='margins_small'>Sunset public project focused on disability assistance. I worked on CI/CD processes such as setting up Github Actions and design choices for the frontend that utilized Swift UI.</p>
 
-      <p>TODO Application</p>
-      <p>Web app used to track tasks, delete tasks, and sort them based on priority.</p>
+      <p className='title'>TODO Application</p>
+      <p className='margins_small'>Web app used to track tasks, delete tasks, and sort them based on priority.</p>
 
-      <p>Austin Leverett Web Resume</p>
-      <p>Website built with React, serving as an online portfolio and resume.</p>
+      <p className='title'>Austin Leverett Web Resume</p>
+      <p className='margins_small'>Website built with React, serving as an online portfolio and resume.</p>
       
 
     </div>
