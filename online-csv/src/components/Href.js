943774b7fe2d6e30d7049c44309db16c61408615
Austin Leverett <71096053+MiliAus@users.noreply.github.com>
2024-11-09 22:07:32 +0000

    quick commit

--- online-csv/src/components/Href.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = Href;
-function Href(props) {
-    return (React.createElement("a", { href: props.link }, props.name));
-}
-;
