c068f408f3106a849bcb94edea70e7e64ade5d87
Austin Leverett <71096053+MiliAus@users.noreply.github.com>
2024-11-10 20:35:23 +0000

    quick commit

---
+++ src/components/Link.tsx
@@ -0,0 +1,9 @@
+import 'src/index.css';
+
+export default function Link() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="m256-240-56-56 384-384H240v-80h480v480h-80v-344L256-240Z"/></svg>
+    );
+};
+
+
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,6 +7,7 @@ import Linkedin from 'src/components/Linkedin';
 import Vito from '../assets/vito_website.png';
 import TODO from '../assets/todo_darkmode.png';
 import Web from '../assets/personal_website.png';
+import Link from 'src/components/Link';
 
 
 const Projects = () => {
@@ -18,19 +19,19 @@ const Projects = () => {
       <div className='project_columns'>
           <div className='projects_body'>
               <div className='mt_1rem'>
-                <a className="title" href="https://github.com/Vito-Research">Vito</a>
+                <a className="title" href="https://github.com/Vito-Research">Vito <Link /></a>
                 <p className='mt_4'>Sunset public project focused on detecting infection in realtime. I worked on CI/CD processes such as setting up Github Actions and design choices for the frontend that utilized Swift UI.</p>
                 <div><img className="project_image" src={Vito} alt="vito website" /></div>
               </div>
             
               <div className='mt_1rem'>
-                <a href="https://github.com/MiliAus/todo-app">TODO Application</a>
+                <a className="title" href="https://github.com/MiliAus/todo-app">TODO Application <Link /></a>
                 <p className='mt_4'>Web app used to track tasks, delete tasks, and sort them based on priority.</p>
                 <div><img className="project_image" src={TODO} alt="vito website" /></div>
               </div>
             
               <div className='mt_1rem'>
-                <a href="https://github.com/MiliAus/online-csv">Austin Leverett Web Resume</a>
+                <a className="title" href="https://github.com/MiliAus/online-csv">Austin Leverett Web Resume <Link /></a>
                 <p className='mt_4'>Website built with React, serving as an online portfolio and resume.</p>
                 <div><img className="project_image" src={Web} alt="vito website" /></div>
               </div>
